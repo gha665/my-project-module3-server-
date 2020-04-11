d146008f493252c9aa56a2da3f1ccfdb7055d827
Evgeny Galitskiy <gha665@gmail.com>
2020-04-11 11:11:32 -0400

    first commit

---
+++ my-project-module3(cloudinary-server)/app.js
@@ -0,0 +1,60 @@
+require("dotenv").config();
+
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const favicon = require("serve-favicon");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const path = require("path");
+const cors = require("cors");
+
+mongoose
+    .connect("mongodb://localhost/cloudinary-server", { useNewUrlParser: true })
+    .then((x) => {
+        console.log(
+            `Connected to Mongo! Database name: "${x.connections[0].name}"`
+        );
+    })
+    .catch((err) => {
+        console.error("Error connecting to mongo", err);
+    });
+
+const app_name = require("./package.json").name;
+const debug = require("debug")(
+    `${app_name}:${path.basename(__filename).split(".")[0]}`
+);
+
+const app = express();
+
+// Middleware Setup
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Express View engine setup
+
+app.use(
+    require("node-sass-middleware")({
+        src: path.join(__dirname, "public"),
+        dest: path.join(__dirname, "public"),
+        sourceMap: true,
+    })
+);
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
+
+app.use(
+    cors({
+        credentials: true,
+        origin: true,
+        // origin: ["http://localhost:3000", "https://herokuAppDomainURL"]
+    })
+);
+
+app.use("/api", require("./routes/product-routes/product"));
+app.use("/api", require("./routes/file-uploads/fileUpload"));
+
+module.exports = app;
